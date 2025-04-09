4d1ac404c5c299763f5b6835114594d51cab5cf1
Erwan Langlais <erwan.langlais@isen-ouest.yncrea.fr>
2025-04-09 18:00:29 +0200

    Homepage v2 (unfinished)

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 #include <QApplication>
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
 #include <QWidget>
 #include <QPushButton>
-#include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QTextEdit>
-#include "MainWindow.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication app (argc, argv); 
-    MainWindow window;
-    window.show();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QWidget *stMenu = new QWidget;
+    QPushButton *newGame = new QPushButton("Nouvelle Partie");
+    QPushButton *quitGame = new QPushButton("Quitter le jeu");
+    QHBoxLayout *stMenuLayout = new QHBoxLayout(stMenu);
+    stMenuLayout->addWidget(newGame);
+    stMenuLayout->addWidget(quitGame);
     
+    QMainWindow *mainWindow = new QMainWindow(stMenu);
+    mainWindow->setWindowTitle("Main Window");
+    QMenuBar *menuBar = mainWindow->menuBar();
+
+    QMenu *fileMenu = menuBar->addMenu("Fichier");
+    fileMenu->addAction("Nouveau");
+    fileMenu->addAction("Ouvrir le jeu");
+    fileMenu->addAction("Enregistrer");
+    fileMenu->addAction("Quitter", &app, &QApplication::quit);
+
+    stMenu->show();
+    mainWindow->show();
     return app.exec();
 };
